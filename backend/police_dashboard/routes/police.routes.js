886a529fda6f7512efa264124415afdfbe4e73e4
Kishan Omkar <kishan123406@gmail.com>
2025-09-14 12:20:43 +0530

    QR Code generator and QR Code Scanner done , data is comming after scanning

--- backend/police_dashboard/routes/police.routes.js
+++ backend/police_dashboard/routes/police.routes.js
@@ -2,6 +2,7 @@ import { Router }  from 'express';
 import { body } from 'express-validator';
 import * as policeController from '../controller/police.controller.js';
 import authMiddleware from '../middlewares/auth.middleware.js';
+import * as touristController from '../controller/tourist.controller.js'
 
 
 const router = Router();
@@ -29,5 +30,15 @@ router.get('/policeprofile',
     authMiddleware,
     policeController.policeProfileController
 )
+
+router.post('/create-foreign-tourist',[
+    body('contactInformation.email').isEmail().withMessage('Invalid email format'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+],authMiddleware,touristController.registerForeignTouristController)
+
+router.post('/create-domestic-tourist',[
+    body('contactInformation.email').isEmail().withMessage('Invalid email format'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+],authMiddleware,touristController.registerDomesticTouristController)
 
 export default router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import PoliceRegister from './police_dashboard/pages/PoliceRegister';
+// import Test from './police_dashboard/components/QRreader';
+import QRCamera from './police_dashboard/components/QRreader';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/police/registerpolice" element={<PoliceRegister />} />
+        <Route path='/' element={<QRCamera />} />
       </Routes>
     </Router>
   );
